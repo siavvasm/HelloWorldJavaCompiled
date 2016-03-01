d58e31fcff242c2b0adf0b1eac6f57a1150af7e7
Miltiadis Siavvas <miltos.siavvas@gmail.com>
2016-03-01 21:16:07 +0200

    First

---
+++ src/main/java/miltos/diploma/App.java
@@ -0,0 +1,13 @@
+package miltos.diploma;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
