5f59cf8295b2ca33052bd777c451906fd6f3a231
siavvasm <siavvasm@iti.gr>
2020-11-27 16:03:10 +0200

    New Commit

---
+++ src/main/java/miltos/diploma/ClassToCheck.java
@@ -0,0 +1,19 @@
+package miltos.diploma;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClassToTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "This is a simple code snippet!" );
+		System.out.println( "Hello World!" );
+		System.out.println( "This is a simple code snippet!" );
+		
+    }
+    
+
+}
